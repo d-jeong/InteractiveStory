803a10d07e89136b295e653846c0877e04a65a1b
yechanjeong <yechanjeong@yahoo.com>
2016-05-19 17:35:45 -0400

    Finished the app!

--- app/src/main/java/com/davidjeong/interactivestory/ui/MainActivity.java
+++ app/src/main/java/com/davidjeong/interactivestory/ui/MainActivity.java
@@ -39,4 +39,9 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mNameField.setText("");
+    }
 }
--- app/src/main/java/com/davidjeong/interactivestory/ui/StoryActivity.java
+++ app/src/main/java/com/davidjeong/interactivestory/ui/StoryActivity.java
@@ -1,16 +1,32 @@
 package com.davidjeong.interactivestory.ui;
 
+
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.davidjeong.interactivestory.R;
 import com.davidjeong.interactivestory.model.Page;
+import com.davidjeong.interactivestory.model.Story;
 
 public class StoryActivity extends AppCompatActivity {
 
     public static final String TAG = StoryActivity.class.getSimpleName();
+
+    private Story mStory = new Story();
+    private ImageView mImageView;
+    private TextView mTextView;
+    private Button mChoice1;
+    private Button mChoice2;
+    private Page mCurrentPage;
+
+    private String mName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +34,62 @@ public class StoryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_story);
 
         Intent intent = getIntent();
-        String name = intent.getStringExtra(getString(R.string.key_name));
+        mName = intent.getStringExtra(getString(R.string.key_name));
 
-        if (name == null) {
-            name = "Friend";
+        if (mName == null) {
+            mName = "Friend";
         }
-        Log.d(TAG, name);
+        Log.d(TAG, mName);
+
+        mImageView = (ImageView) findViewById(R.id.storyImageView);
+        mTextView = (TextView) findViewById(R.id.storyTextView);
+        mChoice1 = (Button) findViewById(R.id.choiceButton1);
+        mChoice2 = (Button) findViewById(R.id.choiceButton2);
+
+        loadPage(0);
+    }
+
+
+    private void loadPage(int choice) {
+        mCurrentPage = mStory.getPage(choice);
+
+        Drawable drawable = getResources().getDrawable(mCurrentPage.getImageId());
+        mImageView.setImageDrawable(drawable);
+
+        String pageText = mCurrentPage.getText();
+        // Add the name if the placeholder is included, will not add if there is no placeholder
+        pageText = String.format(pageText, mName, "%1$s");
+        mTextView.setText(pageText);
+
+        if (mCurrentPage.isFinal()) {
+            mChoice1.setVisibility(View.INVISIBLE);
+            mChoice2.setText("PLAY AGAIN");
+
+            mChoice2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    finish();
+                }
+            });
+        }
+        else {
+            mChoice1.setText(mCurrentPage.getChoice1().getText());
+            mChoice2.setText(mCurrentPage.getChoice2().getText());
+
+            mChoice1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    loadPage(mCurrentPage.getChoice1().getNextPage());
+                }
+            });
+
+            mChoice2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    loadPage(mCurrentPage.getChoice2().getNextPage());
+                }
+            });
+        }
+
     }
 }
